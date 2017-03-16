2b3cf117cc4b2c95bce824703add040d2ce9a58e
Asma Rafiq <asmarafiq@n149-p71.kthopen.kth.se>
2017-03-16 16:41:55 +0100

    help GUI

--- othello-master/android-othello/app/src/main/java/se/kth/sda/othello/MenuActivity.java
+++ othello-master/android-othello/app/src/main/java/se/kth/sda/othello/MenuActivity.java
@@ -4,24 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-
+import android.support.v7.app.AppCompatActivity;
 import se.kth.sda.othello.MainActivity;
 import se.kth.sda.othello.R;
 
-public class MenuActivity extends Activity {
+public class MenuActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button btn = (Button) findViewById(R.id.help);
+
     }
 
     /** Called when the user clicks the button */
-    public void startHumanGame(View view) {
+     public void startHumanGame(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra(MainActivity.GAME_TYPE, MainActivity.GAME_HUMAN);
-        startActivityForResult(intent, 0);
+       startActivityForResult(intent, 0);
     }
+
+     public void help(View v) {
+
+         Intent itn = new Intent(this,HelpActivity.class);
+         startActivity(itn);
+     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
