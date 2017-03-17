b7186e5c1760689084c1be34e72b222cefcd44f2
Shuai <tmpuser-10209@kth.se>
2017-03-17 14:10:26 +0100

    something wrong in build.sh

--- othello-master/othello-service/build.sh
+++ othello-master/othello-service/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p classes
-javac -d classes -cp lib\gson-2.3.1.jar;lib\spark-core-2.5.4.jar;lib\javax.servlet-api-3.1.0.jar src\se\kth\sda\othello\service\*.java
+javac -d classes -cp lib/gson-2.3.1.jar:lib/spark-core-2.5.4.jar:lib/javax.servlet-api-3.1.0.jar src/se/kth/sda/othello/service/*.java
