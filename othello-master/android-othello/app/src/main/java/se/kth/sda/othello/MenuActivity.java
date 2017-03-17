2e5c597359d86cbb9f9903a24b6c38ab34097dda
Shuai <tmpuser-10209@kth.se>
2017-03-17 15:45:06 +0100

    fix a bug

--- othello-master/android-othello/app/src/main/java/se/kth/sda/othello/MenuActivity.java
+++ othello-master/android-othello/app/src/main/java/se/kth/sda/othello/MenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import se.kth.sda.othello.MainActivity;
@@ -18,7 +19,7 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Button btn = (Button) findViewById(R.id.help);
+        ImageButton btn = (ImageButton) findViewById(R.id.help);
 
     }
 
