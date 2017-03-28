ea6c6e44f0a3d61d69b73309ffafd3e93080776d
Samer Obid <samerobid@gmail.com>
2017-03-28 08:51:18 +0200

    Replacing the login fields in the welcome screen to a button, and add a registration button.

--- othello-master/android-othello/app/src/main/java/se/kth/sda/othello/RegActivity.java
+++ othello-master/android-othello/app/src/main/java/se/kth/sda/othello/RegActivity.java
@@ -1,7 +1,9 @@
 package se.kth.sda.othello;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Samer Obid on 2017-03-24.
@@ -13,7 +15,11 @@ public class RegActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+    }
+    public void reg (View v) {
 
+        Intent itn = new Intent(this,RegistrationActivity.class);
+        startActivity(itn);
 
 
     }
