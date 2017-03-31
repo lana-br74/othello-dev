71bd76f1b520bd180d43295b1a42ee059429f4be
Lana <eng.lana@hotmail.com>
2017-03-31 16:32:49 +0200

    adding registration and login services

--- othello-master/othello-service/src/se/kth/sda/othello/service/User.java
+++ othello-master/othello-service/src/se/kth/sda/othello/service/User.java
@@ -1,17 +1,75 @@
 package se.kth.sda.othello.service;
 
+/**
+ * This class represent a user of the game with all relating information. in addition to 
+ * setter and getter methods , there are methods to change the user coins and number of his loses and wins.
+ * @author lana
+ *
+ */
 public class User {
-    public String login;
-    public String name;
-    public String surname;
-    public int wins;
-    public int loses;
+	private final String userName;
+	private final String password;
+	private final String email;
+	private String name;
+	private int coins;
+	private int wins;
+	private int loses;
 
-    public User(String login, String name, String surname, int wins, int loses) {
-	this.login = login;
-	this.name = name;
-	this.surname = surname;
-	this.wins = wins;
-	this.loses = loses;
-    }
+	public User(String email,String userName, String password,String name){
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.name = name;
+		this.coins = 100;
+		this.wins = 0;
+		this.loses = 0;
+	}
+
+	public String getUserName(){
+		return this.userName;
+	}
+
+	public String getPassword(){
+		return this.password;
+	}
+
+	public String getEmail(){
+		return this.email;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public int getCoins(){
+		return this.coins;
+	}
+
+	public void addCoins(int coinsToAdd){
+		this.coins += coinsToAdd;
+	}
+
+	public void subtractCoins(int coinsToAbstract){
+		this.coins -= coinsToAbstract;
+	}
+
+	public int getWins(){
+		return this.wins;
+	}
+
+	public void addWins(){
+		this.wins +=1;
+	}
+
+	public int getLoses(){
+		return this.loses;
+	}
+
+	public void addLoses(){
+		this.loses+=1;
+	}
 }
